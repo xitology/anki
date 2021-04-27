7e6130a215176dbc363233671aaf76a903122735
Damien Elmes <gpg@ankiweb.net>
2021-04-27 11:53:04 +1000

    add example of how a slotted component could be invoked from js
    - define a generic component that takes a list of components, and
    provides them as a slot to another provided component

--- ts/sveltelib/ButtonGroup.svelte
+++ ts/sveltelib/ButtonGroup.svelte
@@ -4,74 +4,11 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="typescript">
     import type { ToolbarItem } from "sveltelib/types";
-    import { getContext } from "svelte";
-    import { nightModeKey } from "sveltelib/contextKeys";
-
-    export let id: string | undefined;
-    export let className = "";
+    import HideableComponentsAsSlot from "./HideableComponentsAsSlot.svelte";
+    import ButtonGroup2 from "./ButtonGroup2.svelte";
     export let items: ToolbarItem[];
-    export let size: number;
-
-    $: sizeString = size ? `--toolbar-size: ${size}px;` : undefined;
-
-    function filterHidden({ hidden = false, ...props }) {
-        return props;
-    }
-
-    const nightMode = getContext(nightModeKey);
 </script>
 
-<style lang="scss">
-    div {
-        display: flex;
-        justify-items: start;
-        flex-wrap: var(--toolbar-wrap);
-
-        padding: calc(var(--toolbar-size) / 10);
-        margin: 0;
-
-        /* :is() would be better suited here */
-        > :global(button),
-        > :global(select) {
-            border-radius: calc(var(--toolbar-size) / 7.5);
-
-            &:not(:nth-of-type(1)) {
-                border-top-left-radius: 0;
-                border-bottom-left-radius: 0;
-            }
-
-            &:not(:nth-last-of-type(1)) {
-                border-top-right-radius: 0;
-                border-bottom-right-radius: 0;
-            }
-        }
-
-        &.border-overlap-group {
-            > :global(button),
-            > :global(select) {
-                margin-left: -1px !important;
-            }
-        }
-
-        &.gap-group {
-            > :global(button),
-            > :global(select) {
-                margin-left: 1px !important;
-            }
-        }
-    }
-</style>
-
-<div
-    {id}
-    class={className}
-    class:border-overlap-group={!nightMode}
-    class:gap-group={nightMode}
-    style={sizeString}
-    dir="ltr">
-    {#each items as button}
-        {#if !button.hidden}
-            <svelte:component this={button.component} {...filterHidden(button)} />
-        {/if}
-    {/each}
-</div>
+<HideableComponentsAsSlot
+    target={{ ...$$restProps, component: ButtonGroup2 }}
+    {items} />
---
+++ ts/sveltelib/HideableComponentsAsSlot.svelte
@@ -0,0 +1,21 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+<script lang="typescript">
+    import type { DynamicSvelteComponent } from "sveltelib/dynamicComponents";
+    export let target: DynamicSvelteComponent;
+    export let items: DynamicSvelteComponent[];
+
+    function filterHidden({ hidden = false, ...props }) {
+        return props;
+    }
+</script>
+
+<svelte:component this={target.component} {...target}>
+    {#each items as button}
+        {#if !button.hidden}
+            <svelte:component this={button.component} {...filterHidden(button)} />
+        {/if}
+    {/each}
+</svelte:component>
